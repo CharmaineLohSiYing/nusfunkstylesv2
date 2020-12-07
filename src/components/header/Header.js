73ab0d70e189671008f0e9afa7f52dc0e4c2fbc1
charmainenotlowloh <e0203409@u.nus.edu>
2020-12-07 15:23:20 +0800

    responsive navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,29 +3,22 @@ import styles from './Header.module.css'
 import {Navbar, Nav, NavDropdown} from 'react-bootstrap'
 import { FiMenu } from "react-icons/fi";
 
-const white = "rgba(252,252,252,0.75)";
-
 const Header = () => {
 
     // const [isNavbarCollapsed, setIsNavbarCollapsed] = useState(true);
 
     return (
-        // <div className={styles.header}>
-        //     <img src={"/images/Logo.png"} alt="logo"/>
-        //     <div className={styles.circle}></div>
-        // </div>
-        <Navbar bg="dark" expand="md">
+        <Navbar bg="dark" expand="md" className={styles.header}>
             <Navbar.Brand href="#home"><img src={"/images/Logo.png"} alt="logo"/></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav">
                 <FiMenu className="white" size={24}/>
             </Navbar.Toggle>
             <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-                {/* <Nav className="mr-auto"> */}
-                <Nav className="mr-sm-2">
-                <Nav.Link href="#home"><h4 className="white montserrat_semi_bold">HOME</h4></Nav.Link>
-                <Nav.Link href="#home"><h4 className="white montserrat_semi_bold">COMMUNITY</h4></Nav.Link>
-                <Nav.Link href="#home"><h4 className="white montserrat_semi_bold">EVENTS</h4></Nav.Link>
-                <Nav.Link href="#home"><h4 className="white montserrat_semi_bold">CONTACT US</h4></Nav.Link>
+                <Nav>
+                <Nav.Link href="#home" active={true} className="px-lg-4 px-md-2"><h4 className="white montserrat_semi_bold px-md-2">HOME</h4></Nav.Link>
+                <Nav.Link href="#home" className="px-lg-4 px-md-2"><h4 className="white montserrat_semi_bold px-md-2">COMMUNITY</h4></Nav.Link>
+                <Nav.Link href="#home" className="px-lg-4 px-md-2"><h4 className="white montserrat_semi_bold px-md-2">EVENTS</h4></Nav.Link>
+                <Nav.Link href="#home" className="px-lg-4 px-md-2"><h4 className="white montserrat_semi_bold px-md-2">CONTACT US</h4></Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -4,6 +4,7 @@ const Homepage = () => {
     return (
         <div className={styles.homepage}>
             <video src="/images/Homepage Video.mp4" autoPlay loop muted/>
+            {/* <div class="home-text1">A community of funk style dancers<br>connecting through music and dance.</div> */}
         </div>
     )
 }
