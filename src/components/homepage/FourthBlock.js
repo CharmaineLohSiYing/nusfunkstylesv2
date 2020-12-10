13e1aa59f6ced77511796fe3bc0ae403e83cc7a1
charmainenotlowloh <e0203409@u.nus.edu>
2020-12-10 18:46:35 +0800

    fourth block

--- src/components/homepage/FourthBlock.js
+++ src/components/homepage/FourthBlock.js
@@ -5,23 +5,23 @@ import Block from '../common/Block';
 export default function FourthBlock() {
     return (
         <Block imageBackground="images/homepage/selfie.jpg" alt="Showcase">
-            <div className={styles.text}>
-                {/* <div>Vision</div>
-                <div className="vmd-text">To be a vibrant and prominent tertiary Funk Style club in<br/> Singapore that
+            <div className={styles.text + " montserrat_regular"}>
+                <div className={styles.subtitle + " orange"}>Vision</div>
+                <div>To be a vibrant and prominent tertiary Funk Style club in<br/> Singapore that
                     cultivates creativity and individuality by<br/> creating an environment where learning is without limits
                     <br/>
                     <br/>
                 </div>
-                <div>Mission</div>
-                <div className="vmd-text">To bring diversity to NUS by cultivating a community of<br/> Funkstyle dancers in NUS,
+                <div className={styles.subtitle + " orange"}>Mission</div>
+                <div>To bring diversity to NUS by cultivating a community of<br/> Funkstyle dancers in NUS,
                     providing students with affordable<br/> and quality Popping and Locking classes, and a gateway to<br/> the
                     local Funk Style Community
                     <br/>
                     <br/>
                 </div>
-                <div>Direction</div>
-                <div className="vmd-text">No audition, promotion towards funk related events and to<br/>
-                    the scene in Singapore</div> */}
+                <div className={styles.subtitle + " orange"}>Direction</div>
+                <div>No audition, promotion towards funk related events and to<br/>
+                    the scene in Singapore</div>
             </div>
         </Block>
     )
