af9e1e2971e4a25f73d4c597d1064d5e5f656856
charmainenotlowloh <e0203409@u.nus.edu>
2021-01-28 10:56:15 +0800

    react lazy loading routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import React, { Suspense, lazy } from 'react';
 import Header from './components/header/Header'
 import Homepage from './components/homepage/HomepageMain'
-import Community from './components/community/CommunityMain'
-import Events from "./components/events/EventsMain";
-import ContactMain from "./components/contact/ContactMain";
+const Community = lazy(() => import('./components/community/CommunityMain'));
+const Events = lazy(() => import('./components/events/EventsMain'));
+const ContactMain = lazy(() => import('./components/contact/ContactMain'));
 import Footer from './components/footer/Footer'
 import Filler from './components/Filler'
 
@@ -24,23 +25,25 @@ function App() {
             <Header/>
           </header>
           <main>
+          <Suspense fallback={<div>Loading...</div>}>
             <Switch>
-                <Route path="/home">
-                  <Homepage/>
-                </Route>
-                <Route path="/community">
-                  <Community />
-                </Route>
-                <Route path="/events">
-                  <Events/>
-                </Route>
-                <Route path="/contact">
-                  <ContactMain/>
-                </Route>
-                <Route path="/">
-                  <Redirect to="/home" />
-                </Route>
-            </Switch>
+                  <Route path="/home">
+                    <Homepage/>
+                  </Route>
+                  <Route path="/community">
+                    <Community />
+                  </Route>
+                  <Route path="/events">
+                    <Events/>
+                  </Route>
+                  <Route path="/contact">
+                    <ContactMain/>
+                  </Route>
+                  <Route path="/">
+                    <Redirect to="/home" />
+                  </Route>
+              </Switch>
+          </Suspense>
           </main>
           <Footer/>
           <Filler/>
